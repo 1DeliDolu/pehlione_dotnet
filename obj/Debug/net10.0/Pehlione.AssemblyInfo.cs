1c711e5014b725413622cb9b272ab78d06b6ea4f
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2026-02-25 21:05:03 +0100

    Add move-products flow to unblock category deletion

--- obj/Debug/net10.0/Pehlione.AssemblyInfo.cs
+++ obj/Debug/net10.0/Pehlione.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pehlione")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15520488c7ebdba93727e357ba82f6456cbc2dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+131955234950a6b4b95f33edf20e3c8fca9fe6b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pehlione")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pehlione")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
